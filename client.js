70600142e9c383be60bfc09cbb996f11be34bd95
abbygold00 <abiakinfenwa@gmail.com>
2017-07-31 15:20:00 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,1 +1,15 @@
 
+
+var toggleBtn = document.getElementById("toggle-btn");
+toggleBtn.addEventListener("click", myToggleFunction);
+
+function myToggleFunction() {
+    var x = document.getElementById('myBio');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+        toggleBtn.innerHTML = "hide my bio";
+    } else {
+        x.style.display = 'none';
+        toggleBtn.innerHTML = "show my bio";
+    }
+}
